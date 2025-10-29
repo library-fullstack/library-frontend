2413ef4301f0c534d8aabdb6095a7dd388d0070d
hoaug-tran <92006941+hoaug-tran@users.noreply.github.com>
2025-10-29 11:05:13 +0700

    fix(analytics): use @vercel/analytics/react instead of next in Vite app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import AppRoutes from "./app/routes/AppRoutes";
 import { SpeedInsights } from "@vercel/speed-insights/react";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const App: React.FC = () => {
   return (
