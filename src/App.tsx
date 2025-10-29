c3bc176d7b681e018736f0d1a34e2cc6491f7f2c
hoaug-tran <92006941+hoaug-tran@users.noreply.github.com>
2025-10-29 11:02:35 +0700

    chore(analytics): add Vercel Analytics integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import AppRoutes from "./app/routes/AppRoutes";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/next";
 
 const App: React.FC = () => {
   return (
     <>
       <AppRoutes />
       <SpeedInsights />
+      <Analytics />
     </>
   );
 };
