09c8bb34e5362687d3acae61544d4e6ee5969e45
hoaug-tran <hoaug@duck.com>
2025-11-19 08:43:45 +0700

    chore: test vercel-build

--- src/widgets/featured-books/FeaturedBooks.tsx
+++ src/widgets/featured-books/FeaturedBooks.tsx
@@ -465,9 +465,7 @@ const FeaturedBooks: React.FC = () => {
             style={{ paddingBottom: 50 }}
           >
             {books.map((book) => (
-              <SwiperSlide key={book.id}>
-                <Box sx={{ height: "100%" }}>{CardItem(book)}</Box>
-              </SwiperSlide>
+              <SwiperSlide key={book.id}>{CardItem(book)}</SwiperSlide>
             ))}
           </Swiper>
 
